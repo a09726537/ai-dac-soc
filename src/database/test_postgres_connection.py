f2d32c77db9535cfb3871f56e28322e25f7fe405
William Kandolo <a09726537@unet.univie.ac.at>
2026-05-11 03:28:17 +0200

    Add PostgreSQL connection test

---
+++ src/database/test_postgres_connection.py
@@ -0,0 +1,16 @@
+import psycopg2
+
+conn = psycopg2.connect(
+    host="localhost",
+    port=5432,
+    database="aidac",
+    user="william",
+    password="Oracle2020"
+)
+
+cur = conn.cursor()
+cur.execute("SELECT version();")
+print(cur.fetchone()[0])
+
+cur.close()
+conn.close()
